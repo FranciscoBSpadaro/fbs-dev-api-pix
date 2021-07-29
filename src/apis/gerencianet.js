858f7edd587fc3897bb7e4506551ca363feb7857
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-29 20:22:34 -0300

    v1

--- src/apis/gerencianet.js
+++ src/apis/gerencianet.js
@@ -1,22 +1,24 @@
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
 
+// carrega o certificado de forma Sincrona
 const cert = fs.readFileSync(
   path.resolve(__dirname, `../../certs/${process.env.GN_CERT}`)
-);
-
+)
+// adiciona certificado na request https
 const agent = new https.Agent({
   pfx: cert,
   passphrase: ''
-});
-
+})
+// converte o buffer do clientID e clientSecret para base64 para poder gerar o request  do token
 const authenticate = ({ clientID, clientSecret }) => {
-  const credentials = Buffer.from(
+  let credentials = Buffer.from(
     `${clientID}:${clientSecret}`
-  ).toString('base64');
+  ).toString('base64')
 
+  // enviando a request post com todos os dados  para receber o token de acesso
   return axios({
     method: 'POST',
     url: `${process.env.GN_ENDPOINT}/oauth/token`,
@@ -24,17 +26,18 @@ const authenticate = ({ clientID, clientSecret }) => {
       Authorization: `Basic ${credentials}`,
       'Content-Type': 'application/json'
     },
+    // corpo do request em json do tipo client credentials do oauth2
     httpsAgent: agent,
     data: {
       grant_type: 'client_credentials'
     }
-  });
-};
+  })
+}
 
-
+// variavel que armazena o token de acesso para ser exportado para  as rotas de request
 const GNRequest = async (credentials) => {
-  const authResponse = await authenticate(credentials);
-  const accessToken = authResponse.data?.access_token;
+  let authResponse = await authenticate(credentials)  
+  let accessToken = authResponse.data?.access_token  // resposta com access token recebe o nome access_token
 
   return axios.create({
     baseURL: process.env.GN_ENDPOINT,
@@ -43,7 +46,7 @@ const GNRequest = async (credentials) => {
       Authorization: `Bearer ${accessToken}`,
       'Content-Type': 'application/json'
     }
-  });
+  })
 }
 
-module.exports = GNRequest;
+module.exports = GNRequest
